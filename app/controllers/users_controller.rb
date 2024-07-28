50be1c08d03b7413a95e895042ae01e9189f388e
João Paulo Just Peixoto <just1982@gmail.com>
2024-07-28 12:04:27 -0300

    fix: Remove password_digest from UsersController responses

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,12 +5,12 @@ class UsersController < ApplicationController
   def index
     @users = User.all
 
-    render json: @users
+    render json: @users, except: [:password_digest]
   end
 
   # GET /users/1
   def show
-    render json: @user
+    render json: @user, except: [:password_digest]
   end
 
   # POST /users
@@ -18,7 +18,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      render json: @user, status: :created, location: @user
+      render json: @user, except: [:password_digest], status: :created, location: @user
     else
       render json: @user.errors, status: :unprocessable_entity
     end
@@ -27,7 +27,7 @@ class UsersController < ApplicationController
   # PATCH/PUT /users/1
   def update
     if @user.update(user_params)
-      render json: @user
+      render json: @user, except: [:password_digest]
     else
       render json: @user.errors, status: :unprocessable_entity
     end
