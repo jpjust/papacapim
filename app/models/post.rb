312a80a4d146323ddac4e5c45e30a9854383d5cb
João Paulo Just Peixoto <just1982@gmail.com>
2024-07-28 12:07:23 -0300

    feat: Add validations to User and Post models

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ApplicationRecord
+
+  validates_presence_of :user_id, :message
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
 
   has_secure_password
 
+  validates_presence_of :login, :name
+
+  validates_uniqueness_of :login
+
 end
