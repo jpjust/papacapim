79099fac861d47b6cc4c81304b3fd314caaf4dbc
João Paulo Just Peixoto <just1982@gmail.com>
2024-08-01 15:39:10 -0300

    Update.sh

--- update.sh
+++ update.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
-sudo chown -R just: * .*
+DIR=$(pwd)
+sudo chown -R just: $DIR
 git pull
 git reset --hard
-bundle install
-RAILS_ENV=production bundle exec rake db:migrate
-sudo chown -R www-data: * .*
+/opt/rbenv/shims/bundle install
+RAILS_ENV=production /opt/rbenv/shims/bundle exec rake db:migrate
+sudo chown -R www-data: $DIR
 sudo touch tmp/restart.txt
