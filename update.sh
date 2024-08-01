e54aa2f62666b4ec178aa74f9aee4506c28ca980
João Paulo Just Peixoto <just1982@gmail.com>
2024-08-01 15:12:28 -0300

    fix: Update script now changes permissions of .* files

--- update.sh
+++ update.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-sudo chown -R just: *
+sudo chown -R just: * .*
 git pull
 git reset --hard
 bundle install
 RAILS_ENV=production bundle exec rake db:migrate
-sudo chown -R www-data: *
+sudo chown -R www-data: * .*
 sudo touch tmp/restart.txt
