e02f77ebde3cf0ef07744e03a751642fb7cdbc6d
João Paulo Just Peixoto <just1982@gmail.com>
2024-09-17 14:28:29 -0300

    fix: Implement following relationship

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,18 +5,12 @@ class User < ApplicationRecord
   has_many :sessions, :dependent => :destroy
   has_many :posts, :dependent => :destroy
   has_many :likes, :dependent => :destroy
+  has_many :followers, :class_name => Follower.to_s, :foreign_key => :followed_id, :dependent => :destroy
+  has_many :following, :class_name => Follower.to_s, :foreign_key => :follower_id, :dependent => :destroy
 
   validates :login, :name, :presence => true
   validates :login, :uniqueness => true
 
   default_scope { order(created_at: :desc) }
-
-  def following
-    Follower.where(follower_id: self.id).map(&:followed)
-  end
-
-  def followers
-    Follower.where(followed_id: self.id).map(&:follower)
-  end
 
 end
